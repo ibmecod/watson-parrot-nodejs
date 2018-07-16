b15e04f8092a205b6bf3f7a8eb262d0eb4ddb013
Tim Robinson <timroster@gmail.com>
2018-07-15 17:29:41 -0700

    update for IAM

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 
 var routes = require('./routes/index');
 // not needed for this simple app
